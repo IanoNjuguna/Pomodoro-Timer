20f9e12de61f312d405a2f9457f3de615d3b1d18
IanoNjuguna <inc3099@gmail.com>
2023-01-29 16:23:32 +0300

    update directories

--- src/src.py
+++ src/src.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import time
 import threading
 import tkinter as tk
@@ -10,7 +12,7 @@ class PomodoroTimer:
         self.root = tk.Tk()
         self.root.geometry("600x300")
         self.root.title("Pomodoro Timer by Iano")
-        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="Timer/jpeg"))
+        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="Timer.jpeg"))
 
         self.root.mainloop()
 
